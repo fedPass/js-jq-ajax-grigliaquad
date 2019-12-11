84b0c644d8809f0a8a64f5cd404636e6e1e63b70
fedPass <federica.passanante@gmail.com>
2019-12-11 18:45:59 +0100

    aggiunta verifica per cliccare solo una volta

--- main.js
+++ main.js
@@ -3,30 +3,32 @@ $(document).ready(function(){
     for (var i = 0; i < 36; i++) {
         $('.container').append('<div class="quadrato"></div>');
     }
-
     // Ad ogni click su un quadratino
     $('.quadrato').click(function(){
         var currentSquare = $(this);
-        // parte una chiamata AJAX all'API
-        $.ajax({
-            url : 'https://flynn.boolean.careers/exercises/api/random/int',
-            method : 'GET',
-            success : function (data) {
-                console.log(data.response);
-                // $(currentSquare).html('<span>'+ data.response +'</span>');
-                // se il numero <= 5 il quadratino diventa giallo
-                if (data.response <= 5) {
-                    $(currentSquare).addClass('giallo');
-                } else {
-                // se il numero > 5 il quadratino diventa verde
-                    $(currentSquare).addClass('verde');
+        //se non l'ho già cliccato
+        if (!(currentSquare.hasClass('verde')) && !(currentSquare.hasClass('giallo'))) {
+
+            // parte una chiamata AJAX all'API
+            $.ajax({
+                url : 'https://flynn.boolean.careers/exercises/api/random/int',
+                method : 'GET',
+                success : function (data) {
+                    console.log(data.response);
+                    $(currentSquare).text(data.response);
+                    // se il numero <= 5 il quadratino diventa giallo
+                    if (data.response <= 5) {
+                        $(currentSquare).addClass('giallo');
+                    } else {
+                    // se il numero > 5 il quadratino diventa verde
+                        $(currentSquare).addClass('verde');
+                    }
+                },
+                error : function (err) {
+                alert("E' avvenuto un errore");
+                console.log(err.status);
                 }
-            },
-            error : function () {
-            alert("E' avvenuto un errore");
-            }
-        });
+            });
+        }
     })
-
-
 })
